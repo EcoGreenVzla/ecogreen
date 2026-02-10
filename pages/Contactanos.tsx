e50954ab7243b78e00a9f9f97e001aa401f86264
gamarista1 <tu_correo_configurado_en_github@example.com>
2026-02-10 11:43:35 -0400

    Refact: Mapas - fix deploy

--- pages/Contactanos.tsx
+++ pages/Contactanos.tsx
@@ -150,7 +150,7 @@ const Contactanos: React.FC = () => {
                     scrollWheelZoom={false} 
                     style={{ width: '100%', height: '100%' }}
                 >
-                    {/* Capa de Mapa Oscura (CartoDB Dark Matter) - Gratuita */}
+                    {/* Capa de Mapa Oscura (CartoDB Dark Matter Map) - Gratuita */}
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
