667012d1ea48413523e3ca758b86649b8cbbc226
gamarista1 <tu_correo_configurado_en_github@example.com>
2026-02-13 04:36:31 -0400

    Refact: esquema de navegacion

--- App.tsx
+++ App.tsx
@@ -1,52 +1,57 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './pages/index'; 
 import UnderConstruction from './components/UnderConstruction';
 import ScrollToTop from './components/ScrollToTop';
 
-// Import all the new page components
-import MurosDeContencion from './pages/MurosDeContencion';
-import Vialidad from './pages/Vialidad';
-import Petroleo from './pages/Petroleo';
-import Contactanos from './pages/Contactanos';
-import ControlDeErosion from './pages/ControlDeErosion';
-import CamposDeAplicacionPage from './pages/CamposDeAplicacionPage';
-import CasosDeObrasPage from './pages/CasosDeObrasPage';
+// Rutas Principales
+import MurosDeContencion from './pages/muros-de-contencion';
+import Vialidad from './pages/vialidad';
+import Petroleo from './pages/petroleo';
+import Contactanos from './pages/contactanos';
+import ControlDeErosion from './pages/control-de-erosion';
+import CamposDeAplicacionPage from './pages/campos-de-aplicacion';
+import CasosDeObrasPage from './pages/casos-de-obras';
 
-import SistemasFlexibles from './pages/SistemasFlexibles';
-import SistemasRigidos from './pages/SistemasRigidos';
-import SistemasDrenaje from './pages/SistemasDrenaje';
+// Muros de Contención - Sistemas
+import SistemasFlexibles from './pages/sistemas-de-contencion-flexibles';
+import SistemasRigidos from './pages/sistemas-de-contencion-rigidos';
+import SistemasDrenaje from './pages/sistemas-de-drenaje';
 
-import TierraReforzada from './pages/MurosDeTierraReforzada';
+// Muros de Contención - Flexibles
+import TierraReforzada from './pages/muros-de-tierra-reforzada';
 import Gavion from './pages/muros-de-gavion';
-import GavionReforzado from './pages/GavionReforzado';
-import MurosMixtos from './pages/MurosMixtos';
-import RefuerzoTaludes from './pages/RefuerzoTaludes';
+import GavionReforzado from './pages/muros-de-gavion-reforzados';
+import MurosMixtos from './pages/muros-mixtos';
+import RefuerzoTaludes from './pages/refuerzo-de-taludes-y-terraplenes';
 
-import MurosAnclados from './pages/MurosAnclados';
-import ConcretoArmado from './pages/ConcretoArmado';
-import MurosCiclopeos from './pages/MurosCiclopeos';
-import PilotesMicropilotes from './pages/PilotesMicropilotes';
+// Muros de Contención - Rígidos
+import MurosAnclados from './pages/muros-anclados-y-pantallas-atirantadas';
+import ConcretoArmado from './pages/muros-de-concreto-armado';
+import MurosCiclopeos from './pages/muros-de-ciclopeos';
+import PilotesMicropilotes from './pages/pilotes-y-micropilotes';
 
-import EstabilizacionTaludes from './pages/EstabilizacionTaludes';
-import RefuerzoTerraplenes from './pages/RefuerzoTerraplenes';
-import Reforestacion from './pages/Reforestacion';
-import Canalizaciones from './pages/Canalizaciones';
-import DiquesPresas from './pages/DiquesPresas';
-import ProteccionRiberas from './pages/ProteccionRiberas';
+// Control de Erosión
+import EstabilizacionTaludes from './pages/estabilizacion-de-taludes';
+import RefuerzoTerraplenes from './pages/refuerzo-de-taludes-y-terraplenes'; 
+import Reforestacion from './pages/reforestacion';
+import Canalizaciones from './pages/canalizaciones';
+import DiquesPresas from './pages/diques-y-presas';
+import ProteccionRiberas from './pages/proteccion-de-riberas';
 
-import InfraestructuraUrbana from './pages/InfraestructuraUrbana';
-import ConstruccionCivil from './pages/ConstruccionCivil';
-import ProteccionAmbiental from './pages/ProteccionAmbiental';
-import IndustriaPetroleoGas from './pages/IndustriaPetroleoGas';
-import SectorAgricola from './pages/SectorAgricola';
-import ViasComunicacion from './pages/ViasComunicacion';
-import ObrasHidraulicas from './pages/ObrasHidraulicas';
-import MineriaGeotecnia from './pages/MineriaGeotecnia';
+// Campos de Aplicación
+import InfraestructuraUrbana from './pages/infraestructura-urbana';
+import ConstruccionCivil from './pages/construccion-civil';
+import ProteccionAmbiental from './pages/proteccion-ambiental';
+import IndustriaPetroleoGas from './pages/industria-petroleo-y-gas';
+import SectorAgricola from './pages/sector-agricola';
+import ViasComunicacion from './pages/vias-de-comunicacion';
+import ObrasHidraulicas from './pages/obras-hidraulicas';
+import MineriaGeotecnia from './pages/mineria-geologia-y-geotecnia';
 
+// Petróleo 
 import PetroleoProducto1 from './pages/PetroleoProducto1';
 import PetroleoProducto2 from './pages/PetroleoProducto2';
 import PetroleoProducto3 from './pages/PetroleoProducto3';
@@ -63,7 +68,7 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             
-            {/* Top Level Routes */}
+            {/* Rutas Principales */}
             <Route path="/muros-de-contencion" element={<MurosDeContencion />} />
             <Route path="/vialidad" element={<Vialidad />} />
             <Route path="/petroleo" element={<Petroleo />} />
@@ -71,38 +76,44 @@ const App: React.FC = () => {
             <Route path="/control-de-erosion" element={<ControlDeErosion />} />
             <Route path="/campos-de-aplicacion" element={<CamposDeAplicacionPage />} />
             <Route path="/casos-de-obras" element={<CasosDeObrasPage />} />
+            <Route path="/casos-de-obras/100" element={<UnderConstruction title="OBRA ECOGREEN 100" />} />
+            <Route path="/casos-de-obras/150" element={<UnderConstruction title="OBRA ECOGREEN 150" />} />
+            <Route path="/casos-de-obras/200" element={<UnderConstruction title="OBRA ECOGREEN 200" />} />
+            <Route path="/casos-de-obras/215" element={<UnderConstruction title="OBRA ECOGREEN 215" />} />
 
             {/* Muros de Contención */}
-            <Route path="/muros-de-contencion/sistemas-flexibles" element={<SistemasFlexibles />} />
-            <Route path="/muros-de-contencion/sistemas-rigidos" element={<SistemasRigidos />} />
-            <Route path="/muros-de-contencion/sistemas-drenaje" element={<SistemasDrenaje />} />
-            <Route path="/muros-de-contencion/sistemas-flexibles/tierra-reforzada" element={<TierraReforzada />} />
-            <Route path="/muros-de-contencion/sistemas-flexibles/gavion" element={<Gavion />} />
-            <Route path="/muros-de-contencion/sistemas-flexibles/gavion-reforzado" element={<GavionReforzado />} />
-            <Route path="/muros-de-contencion/sistemas-flexibles/muros-mixtos" element={<MurosMixtos />} />
-            <Route path="/muros-de-contencion/sistemas-flexibles/refuerzo-taludes" element={<RefuerzoTaludes />} />
-            <Route path="/muros-de-contencion/sistemas-rigidos/muros-anclados" element={<MurosAnclados />} />
-            <Route path="/muros-de-contencion/sistemas-rigidos/concreto-armado" element={<ConcretoArmado />} />
-            <Route path="/muros-de-contencion/sistemas-rigidos/muros-ciclopeos" element={<MurosCiclopeos />} />
-            <Route path="/muros-de-contencion/sistemas-rigidos/pilotes-micropilotes" element={<PilotesMicropilotes />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles" element={<SistemasFlexibles />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-rigidos" element={<SistemasRigidos />} />
+            <Route path="/muros-de-contencion/sistemas-de-drenaje" element={<SistemasDrenaje />} />
+            
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles/muros-de-tierra-reforzada" element={<TierraReforzada />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles/muros-de-gavion" element={<Gavion />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles/muros-de-gavion-reforzados" element={<GavionReforzado />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles/muros-mixtos" element={<MurosMixtos />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-flexibles/refuerzo-de-taludes-y-terraplenes" element={<RefuerzoTaludes />} />
+            
+            <Route path="/muros-de-contencion/sistemas-de-contencion-rigidos/muros-anclados-y-pantallas-atirantadas" element={<MurosAnclados />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-rigidos/muros-de-concreto-armado" element={<ConcretoArmado />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-rigidos/muros-ciclopeos" element={<MurosCiclopeos />} />
+            <Route path="/muros-de-contencion/sistemas-de-contencion-rigidos/pilotes-y-micropilotes" element={<PilotesMicropilotes />} />
 
             {/* Control de Erosión */}
-            <Route path="/control-de-erosion/estabilizacion-taludes" element={<EstabilizacionTaludes />} />
-            <Route path="/control-de-erosion/refuerzo-terraplenes" element={<RefuerzoTerraplenes />} />
+            <Route path="/control-de-erosion/estabilizacion-de-taludes" element={<EstabilizacionTaludes />} />
+            <Route path="/control-de-erosion/refuerzo-de-taludes-y-terraplenes" element={<RefuerzoTerraplenes />} />
             <Route path="/control-de-erosion/reforestacion" element={<Reforestacion />} />
             <Route path="/control-de-erosion/canalizaciones" element={<Canalizaciones />} />
-            <Route path="/control-de-erosion/diques-presas" element={<DiquesPresas />} />
-            <Route path="/control-de-erosion/proteccion-riberas" element={<ProteccionRiberas />} />
+            <Route path="/control-de-erosion/diques-y-presas" element={<DiquesPresas />} />
+            <Route path="/control-de-erosion/proteccion-de-riberas" element={<ProteccionRiberas />} />
 
             {/* Campos de Aplicación */}
             <Route path="/campos-de-aplicacion/infraestructura-urbana" element={<InfraestructuraUrbana />} />
             <Route path="/campos-de-aplicacion/construccion-civil" element={<ConstruccionCivil />} />
             <Route path="/campos-de-aplicacion/proteccion-ambiental" element={<ProteccionAmbiental />} />
-            <Route path="/campos-de-aplicacion/industria-petroleo-gas" element={<IndustriaPetroleoGas />} />
+            <Route path="/campos-de-aplicacion/industria-petroleo-y-gas" element={<IndustriaPetroleoGas />} />
             <Route path="/campos-de-aplicacion/sector-agricola" element={<SectorAgricola />} />
-            <Route path="/campos-de-aplicacion/vias-comunicacion" element={<ViasComunicacion />} />
+            <Route path="/campos-de-aplicacion/vias-de-comunicacion" element={<ViasComunicacion />} />
             <Route path="/campos-de-aplicacion/obras-hidraulicas" element={<ObrasHidraulicas />} />
-            <Route path="/campos-de-aplicacion/mineria-geotecnia" element={<MineriaGeotecnia />} />
+            <Route path="/campos-de-aplicacion/mineria-geologia-y-geotecnia" element={<MineriaGeotecnia />} />
 
             {/* Petróleo */}
             <Route path="/petroleo/producto-1" element={<PetroleoProducto1 />} />
